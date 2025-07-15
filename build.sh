f07b5fdf8d00653cd51dc25d4777257ed8748c43
EO <504068014080@tutanota.com>
2025-07-15 14:19:31 +0900

    Revert "Remove build.sh"
    build.sh is used by cloudflare, not users.
    Without build.sh, deployment fails.
    
    This reverts commit a227ddf252b6c9ea157630450974f8708b9a7869.
    
    Signed-off-by: EO <504068014080@tutanota.com>

--- build.sh
+++ build.sh
@@ -22,3 +22,14 @@ echo "Building site with Zola..."
 
 echo "Build completed successfully!"
 echo "Site built to: public/"
+
+#!/bin/sh -e
+
+ZOLA_VERSION="0.20.0"
+ZOLA_ARCHIVE="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+curl -sL "$ZOLA_ARCHIVE" | tar -xz
+
+sed -i "s/CAPTCHA_SITE_KEY_PLACEHOLDER/${CAPTCHA_SITE_KEY}/g" config.toml
+
+./zola build
