c1b162162ce124590d71297b9130f280f6ab3064
EO <504068014080@tutanota.com>
2025-07-15 11:44:19 +0900

    build: downgrade ZOLA_VERSION to 0.19.2
    This change sets ZOLA_VERSION in build.sh from 0.20.0 to 0.19.2.
    Older version resolves `zola  build`  issues and glibc incompatibility
    on Cloudflare.
    
    Later, build.sh will be removed. This commit serves as practice following LibCPS commit standards.
    
    Signed-off-by: EO <504068014080@tutanota.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Space Cubics website build..."
 
-ZOLA_VERSION="0.20.0"
+ZOLA_VERSION="0.19.2"
 ZOLA_ARCHIVE="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 
 echo "Downloading Zola v${ZOLA_VERSION}..."
